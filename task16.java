b523ffc58afb04bc2d75e0f7c261dfece71e032a
RishabhManchanda7 <manchanda.rishabh90@gmail.com>
2025-06-06 17:53:58 +0530

    practice files uploading

---
+++ task16.java
@@ -0,0 +1,11 @@
+package Enumerations;
+    enum color{
+        red,blue,green,yellow
+    }
+    public class task16 {
+        public static void main(String[] args) {
+            color c1 = color.yellow;
+            System.out.println(c1);
+        }
+    }
+
