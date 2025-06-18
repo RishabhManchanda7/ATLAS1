08176c7286b5c066b2cad86903b34b6b64b2fe61
RishabhManchanda7 <manchanda.rishabh90@gmail.com>
2025-06-19 00:46:38 +0530

    testing

--- Task112.java
+++
@@ -1,8 +0,0 @@
-public class Task112{
-    public static void main(String[] args){
-        System.out.println("Driver class called");
-        Superclass sobj = new Superclass();
-        sobj.superMethod();
-        System.out.println("Driver class ended");
-    }
-}
--- day9/src/task1.java
+++
@@ -1,9 +0,0 @@
-package src;
-
-public class task1 {
-    public static void main(String[] args) {
-
-        System.out.println("Hello World");
-        System.out.println("How are you");
-    }
-}
